f95caf9a18e382ec98971cf5b49b9c58668dcd41
Intern Account <intern@xxxeuisx.zxc>
2024-02-02 16:47:42 +0300

    Добавил фото и улучшил стили

--- myapp/authentication/urls.py
+++ myapp/authentication/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
 from .views import register, home, login_view, logout_view
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
     path('register/', register, name='register'),
@@ -7,3 +9,7 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('', home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
